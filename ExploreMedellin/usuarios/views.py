33a8815ce449a95321b1600f5138187f5f940a5f
Juanqu <jfquintero261@gmail.com>
2024-07-30 21:24:03 -0500

    Setting django - Creacion de Usuarios inicial

---
+++ ExploreMedellin/usuarios/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from .forms import UserRegisterForm
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.set_password(form.cleaned_data['password'])
+            user.save()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'usuarios/register.html', {'form': form})
