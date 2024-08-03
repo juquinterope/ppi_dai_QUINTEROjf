c3b7fb72e2effc9cd7958ca5f6aff16dbadb086d
Juanqu <jfquintero261@gmail.com>
2024-08-03 09:30:11 -0500

    add scripts

---
+++ ExploreAntioquia/scripts/limpiar_municipios.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+df = pd.read_csv('C:\\Users\\juanf\\Desktop\\ppi_dai_QUINTEROjf\\ExploreAntioquia\\data\\municipios.csv')
+print(df.head())
+
+# print(df.isnull().count())
+print(df.columns)
+columns = ['Nombre Municipio', 'Tipo Centro Poblado', 'Longitud','Latitud']
+df = df[columns]
+
+# En el dataframe, esta presente no solo el municipio sino tambien
+# todas sus divisiones como: corregimientos o barrios;
+# se tomara en cuenta unicamente el centro del municipio que hace
+# referencia a su 'CABECERA MUNICIPAL'
+df = df[df['Tipo Centro Poblado'] == 'CABECERA MUNICIPAL']
+# Posteriormente la columna no es util
+df = df[['Nombre Municipio', 'Longitud', 'Latitud']]
+print(df.head())
+df.to_csv('../data/municipios_filtrados.csv', index=False)
